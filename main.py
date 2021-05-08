7c00524903beae459b9e202850c859edd427b8a7
pabloga44 <67555151+pabloga44@users.noreply.github.com>
2021-05-08 11:38:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,6 @@ def load_model(route='',key=''):
 #Por Pablo    
 def clean_data(x,y):
     # Elimina las filas de NaN y te dice cuales son los parámetros que han dado error
-    print('Clean')
     nans = [];
     for index in range(len(y)):
 
@@ -175,7 +174,6 @@ def clean_data(x,y):
     return x,y
 
 def get_data(file_x,file_y, clean=True):
-  print('Get')
   # Función que recibe los archivos de texto y devuelve los datos en ndarrays
   # Independiente del número de parámetros
   # Puede devolverlos ya limpios
